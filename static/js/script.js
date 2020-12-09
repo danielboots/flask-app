10c311df13f0084a11d7af4f78cdc993d1c02a14
Dan Boots <thestereojackers@gmail.com>
2020-12-09 15:55:18 +0000

    add task, date picker etc implementation

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,13 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-    $('.collapsible').collapsible();
-     $(".tooltipped").tooltip();
-  });
-
-
-  
+$(document).ready(function () {
+    $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
+});
